4a3c4ef98e8adebe4e645627867fecbb9675d354
Garth Scaysbrook <brooksy4503@gmail.com>
2017-04-20 11:58:16 +0000

    Add chefs destroy and tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.first #change this later
+    @recipe.chef = Chef.last #change this later
     if @recipe.save
       flash[:success] = "Recipe was created successfully!"
       redirect_to recipe_path(@recipe)
