8f0e6f33c72c791ef00f69a2443484a77adea2bf
Garth Scaysbrook <brooksy4503@gmail.com>
2017-05-16 12:59:02 +0000

    Adding real time chat feature with ActionCable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+
+function scrollToBottom(){
+
+  if($('#messages').length > 0) {
+
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+
+  }
+
+}
+$(document).ready(function() {
+
+  scrollToBottom();
+
+});
